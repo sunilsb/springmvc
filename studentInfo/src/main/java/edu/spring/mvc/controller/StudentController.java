978e85978fe8ef852fcc4fd4995dc49b329fd998
sunil <sunilsb.in@gmail.com>
2015-04-15 23:05:06 +0530

    added property binding customization method initBinder

--- studentInfo/src/main/java/edu/spring/mvc/controller/StudentController.java
+++ studentInfo/src/main/java/edu/spring/mvc/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,9 +39,19 @@ public class StudentController {
 		}
 		return new ModelAndView("admissionSuccess");
 	}
+	
 	@ModelAttribute
 	public void addCommonCode(Model model) {
 		logger.debug("enters addCommonCode method");
 		model.addAttribute("message", "***Student Details***");
+	}
+	
+	/*
+	 * For customizing the binding properties
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		logger.debug("***enters initBinder method***");
+		binder.setDisallowedFields(new String [] {"dob","mobile"});
 	}
 }
