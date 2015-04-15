85c0781a924f8b8018fb5b699791cdacb8e9e441
sunil <sunilsb.in@gmail.com>
2015-04-15 17:01:07 +0530

    Updated web.xml file with DispatcherServlet class. Also added two model classes(Student and Address)

---
+++ studentInfo/src/main/java/edu/spring/mvc/model/Address.java
@@ -0,0 +1,35 @@
+package edu.spring.mvc.model;
+
+public class Address {
+	
+	private String city;
+	private String state;
+	private String country;
+	private Long pincode;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Long getPincode() {
+		return pincode;
+	}
+	public void setPincode(Long pincode) {
+		this.pincode = pincode;
+	}
+	
+}
---
+++ studentInfo/src/main/java/edu/spring/mvc/model/Student.java
@@ -0,0 +1,81 @@
+package edu.spring.mvc.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+
+
+public class Student {
+	
+	@NotNull
+	private int id;
+	@Size(min = 3, max = 10)
+	private String firstName;
+	private String lastName;
+	
+	@Past
+	private Date dob;
+	
+	@Email
+	private String email;
+	private Long mobile;
+	private List<String> skills;
+	private Address address;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public Long getMobile() {
+		return mobile;
+	}
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+	public List<String> getSkills() {
+		return skills;
+	}
+	public void setSkills(List<String> skills) {
+		this.skills = skills;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
